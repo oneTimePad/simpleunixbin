a9c5046dc6c046b39704f9a80ee0fbba911ee315
Dylan Herman <dah242@scarletmail.rutgers.edu>
2016-07-30 00:00:26 -0400

    added -a and -l features

--- ls/ls.c
+++ ls/ls.c
@@ -9,12 +9,28 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <string.h>
+#include <pwd.h>
+#include <grp.h>
 #include "colors.h"
 
 #define CURR_DIRECTORY "PWD"
 #define MAX_FULL_PATH 512
+#define MAX_LONG_FORM_PRINT 2000
+#define LEN_MAX_64_SINT 20
 typedef enum {FALSE, TRUE} _BOOL;
 
+#define DIREC "d"
+#define READ "r"
+#define WRITE "w"
+#define EXEC "x"
+#define NONE "-"
+
+
+#define ISTOOLONG(LENGTH) (LENGTH>=MAX_LONG_FORM_PRINT)? TRUE : FALSE
+#define ISD(MODE) (!S_ISREG(MODE))? DIREC: NONE
+#define ISR(MODE,MASK) (MODE & MASK)? READ : NONE
+#define ISW(MODE,MASK) (MODE & MASK)? WRITE : NONE
+#define ISX(MODE,MASK) (MODE & MASK)? EXEC : NONE
 int main(int argc, char* argv[]){
 
 
@@ -22,10 +38,31 @@ int main(int argc, char* argv[]){
     char* file;
 
 
-    if(argc<2) //if no file is specified assume cwd
+    _BOOL show_hidden = FALSE;
+    _BOOL long_form = FALSE;
+
+    int opt;
+    while((opt=getopt(argc,argv,"al"))!=-1){
+        switch(opt){
+
+          case 'l':
+            long_form = TRUE;
+            break;
+          case 'a':
+            show_hidden = TRUE;
+            break;
+          default:
+            fprintf(stderr,"unknown flag %c\n",opt);
+            break;
+
+        }
+    }
+
+    if(argc < optind+1) //if no file is specified assume cwd
       file = secure_getenv(CURR_DIRECTORY);
     else
-      file = argv[1];
+      file = argv[optind];
+
 
     DIR* dir;
 
@@ -34,7 +71,6 @@ int main(int argc, char* argv[]){
       exit(EXIT_FAILURE);
     }
 
-    _BOOL show_hidden = FALSE;
     struct dirent *entry;
 
     while((entry=readdir(dir))!=NULL){ //traverse directory
@@ -54,6 +90,7 @@ int main(int argc, char* argv[]){
         }
 
         char full_path[MAX_FULL_PATH];
+        memset(full_path,0,MAX_FULL_PATH);
         strncpy(full_path,file,dir_len);
         full_path[dir_len]=0x0;
         if(file[dir_len-1]!='/')
@@ -84,11 +121,103 @@ int main(int argc, char* argv[]){
 
         }
 
-        cprintf(BRIGHT,cforeground,cbackground,"%s\n",entry->d_name);
 
+        if(long_form){
+          int cur_output_size =0;
+          char long_form[MAX_LONG_FORM_PRINT];
+
+
+          memset(long_form,0,MAX_LONG_FORM_PRINT);
+          char * ptr = long_form;
+          sprintf(ptr++,"%s",ISD(file_mode));
+          //user mode flags
+          sprintf(ptr,"%s%s%s",ISR(file_mode,S_IRUSR),ISW(file_mode,S_IWUSR),ISX(file_mode,S_IXUSR));
+          ptr = ptr+3;
+          //group mode flags
+          sprintf(ptr,"%s%s%s",ISR(file_mode,S_IRGRP),ISW(file_mode,S_IWGRP),ISX(file_mode,S_IXGRP));
+          ptr = ptr+3;
+          //world mode flags
+          sprintf(ptr,"%s%s%s",ISR(file_mode,S_IROTH),ISW(file_mode,S_IWOTH),ISX(file_mode,S_IXOTH));
+          ptr = ptr+3;
+          cur_output_size = cur_output_size+strlen(long_form);
+          //printf("%s  \n",long_form);
+
+          cur_output_size++;
+          *ptr++ = ' ';
+
+          struct group *grp;
+          struct passwd *pwd;
+
+          //convert uid to actual name
+          errno = 0;
+          if((pwd=getpwuid(file_stat.st_uid))==NULL){
+            if(errno!=0){
+              perror("getpwuid()");
+              exit(EXIT_FAILURE);
+            }
+            //can't find a name for the user so just print number
+            sprintf(ptr++,"%ld",(long)file_stat.st_uid);
+          }
+          else{
+            size_t pw_name_len =strlen(pwd->pw_name);
+            //check if this makes output too long
+            if(ISTOOLONG(pw_name_len+cur_output_size)){
+              fprintf(stderr, "%s\n","Username too long" );
+              exit(EXIT_FAILURE);
+            }
+            //print the username
+            sprintf(ptr,"%s",pwd->pw_name);
+            ptr =ptr+pw_name_len;
+            cur_output_size+pw_name_len;
+          }
+          *ptr++ = ' ';
+          //convert gid to actual name
+          errno = 0;
+          if((grp=getgrgid(file_stat.st_gid))==NULL){
+              if(errno!=0){
+                perror("getgrgid()");
+                exit(EXIT_FAILURE);
+              }
+              //if name not found just print gid
+              sprintf(ptr++,"%ld",(long)file_stat.st_gid);
+          }
+          else{
+            size_t gr_name_len = strlen(grp->gr_name);
+            //check for overflow
+            if(ISTOOLONG(gr_name_len+cur_output_size)){
+              fprintf(stderr,"%s\n","Group name too long");
+              exit(EXIT_FAILURE);
+            }
+            //print group name
+            sprintf(ptr,"%s",grp->gr_name);
+            cur_output_size+gr_name_len;
+            ptr = ptr+gr_name_len;
+          }
+
+          *ptr++=' ';
+
+          //safely fetch the size of the file (protect overflow)
+          char file_size_holder[LEN_MAX_64_SINT];
+          memset(file_size_holder,0,LEN_MAX_64_SINT);
+          sprintf(file_size_holder,"%ld",(long)file_stat.st_size);
+          size_t file_size_len = strlen(file_size_holder);
+          //sanity check
+          if(ISTOOLONG(cur_output_size+file_size_len)){
+            fprintf(stderr,"%s\n", "File size too long\n");
+            exit(EXIT_FAILURE);
+          }
+          ptr+file_size_len;
+          cur_output_size+file_size_len;
+          //sprintf null-terms: safe
+          strcat(long_form,file_size_holder);
+
+          printf("%s ",long_form);
+
+        }
+
+        cprintf(BRIGHT,cforeground,cbackground,"%s\n",entry->d_name);
     }
 
-    printf("\n");
     if(errno!=0){
       perror("readdir_r()");
       exit(EXIT_FAILURE);
